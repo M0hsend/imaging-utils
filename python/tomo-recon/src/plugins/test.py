a192a21fc775c8419dcb0a048ec7be7b85dd04c5
Mark Basham <mark.basham@diamond.ac.uk>
2014-05-21 00:02:51 +0100

    Added the basis for a simple test runner

---
+++ python/tomo-recon/src/plugins/test.py
@@ -0,0 +1,30 @@
+'''
+Created on 20 May 2014
+
+@author: ssg37927
+'''
+import unittest
+import utils
+
+class GeneralTestCase(unittest.TestCase):
+    def __init__(self, methodName, param1):
+        super(GeneralTestCase, self).__init__(methodName)
+
+        self.param1 = param1
+
+    def runTest(self):
+        filt = utils.load_filter_plugin(self.param1)
+        self.assertEqual("preproceesing filter test", filt.preprocess())
+        self.assertEqual("processing filter test", filt.process())
+        self.assertEqual("postprocessing filter test", filt.postprocess())
+
+
+def load_tests(filter_names):
+    test_cases = unittest.TestSuite()
+    for filter_name in filter_names:
+        test_cases.addTest(GeneralTestCase('runTest', filter_name))
+    return test_cases
+
+def run_test(filter_name):
+    suite = load_tests([filter_name])
+    unittest.TextTestRunner(verbosity=2).run(suite)
---
+++ python/tomo-recon/src/run_test.py
@@ -0,0 +1,9 @@
+'''
+Created on 20 May 2014
+
+@author: ssg37927
+'''
+
+import plugins.test as pt
+
+pt.run_test("filtertest")
